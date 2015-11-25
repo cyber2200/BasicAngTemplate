e821d36e00b6834b08137b45c93f59cf99915b30
Ran Argaman <ranware2200@yahoo.com>
2015-11-25 12:18:01 -0800

    Updating...

--- api.php
+++ api.php
@@ -1,18 +1,12 @@
 <?php
 if (isset($_GET['func']) && $_GET['func'] == 'getData') {
-	$data = array(
-		array('id' => '1', 'name' => 'Test 1'),
-		array('id' => '2', 'name' => 'Test 2'),
-		array('id' => '3', 'name' => 'Test 3'),
-		array('id' => '4', 'name' => 'Test 4'),
-		array('id' => '5', 'name' => 'Test 5'),
-		array('id' => '6', 'name' => 'Test 6'),
-		array('id' => '7', 'name' => 'Test 7'),
-	);
-	echo json_encode($data);
+	$data = file_get_contents('./data/db.json');
+	echo $data;
 } elseif (isset($_GET['func']) && $_GET['func'] == 'addUser') {
 	$payload = file_get_contents('php://input');
 	$payloadData = json_decode($payload, true);
 	$data = array('ret' => 'OK', 'test' => $payloadData['name']);
+	$jsonData = file_get_contents('./data/db.json');
+	$data = json_decode($jsonData, true);
 	echo json_encode($data);	
 }
