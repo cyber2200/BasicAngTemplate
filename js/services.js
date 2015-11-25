009d2669caa542a48b025d813658ace28f7ff8f6
Ran Argaman <ranware2200@yahoo.com>
2015-11-25 09:01:29 -0800

    Updating..

--- js/services.js
+++ js/services.js
@@ -1,19 +1,7 @@
-var dataServices = angular.module('dataServices', ['ngResource']);
+var phonecatServices = angular.module('dataServices', ['ngResource']);
 
-dataServices.factory('Data', ['$resource', function($resource){
+phonecatServices.factory('Data', ['$resource', function($resource){
 	return $resource('./api.php', {}, {
 		query: {method:'GET', params:{'func' : 'getData'}, isArray:true},
 	});
 }]);
-dataServices.factory('DataService', function(){
-	return function(msg) {
-		alert(msg);
-	}
-});
-dataServices.factory('DataService1', function(){
-	return {
-		t1 : function(){
-			console.log('t1');
-		}
-	}
-});
