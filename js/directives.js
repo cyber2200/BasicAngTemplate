10d50b6232c1e253f3eea2fff5bc47894a135a23
Ran Argaman <ran.a@zend.com>
2015-12-03 16:32:05 +0200

    Updating..

--- js/directives.js
+++ js/directives.js
@@ -1,6 +1,6 @@
 var crudDirectives = angular.module('crudDirectives', []);
-crudDirectives.directive('myTest', function(){
+crudDirectives.directive('crudUserForm', function(){
 	return {
-		templateUrl : 'templates/test.html'
+		templateUrl : 'templates/userForm.html'
 	}
 });
