fcf1522abc890fb1026e11c8591a746a8671e8f9
Ran Argaman <ran.a@zend.com>
2015-12-06 17:31:34 +0200

    Updating...

--- js/directives.js
+++ js/directives.js
@@ -1,6 +1,20 @@
 var crudDirectives = angular.module('crudDirectives', []);
 crudDirectives.directive('crudUserForm', function(){
 	return {
+		restrict : 'E',
 		templateUrl : 'templates/userForm.html'
+	}
+});
+crudDirectives.directive('crudTest', function(){
+	return {
+		restrict : 'E',
+		templateUrl : 'templates/test.html',
+		'controller' : function(ngToast) {
+			this.test = 'Test111';
+			this.test1 = function() {
+				ngToast.create('Just a test...');
+			}
+		},
+		'controllerAs' : 'testCtrl'
 	}
 });
